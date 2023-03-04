66fa2b79db4b4bbe3f727eeee131793c43150023
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-03-04 08:00:25 -0300

    css global

---
+++ src/styles/theme.js
@@ -0,0 +1,33 @@
+export default {
+  light: {
+    backgroundBase: '#f9f9f9',
+    backgroundLevel1: '#ffffff',
+    backgroundLevel2: '#f0f0f0',
+    borderBase: '#e5e5e5',
+    textColorBase: '#222222',
+  },
+  dark: {
+    backgroundBase: '#181818',
+    backgroundLevel1: '#202020',
+    backgroundLevel2: '#313131',
+    borderBase: '#383838',
+    textColorBase: '#FFFFFF',
+  },
+};
+/* 
+export default {
+  COLORS: {
+    BACKGROUND_900: "#232129",
+    BACKGROUND_800: "#312E38",
+    BACKGROUND_700: "#3E3B47",
+
+    WHITE: "#F4EDE8",
+    ORANGE: "#FF9000",
+
+    GRAY_100: "#999591",
+    GRAY_300: "#666360",
+
+    RED: "#FF002E"
+  }
+}
+*/
