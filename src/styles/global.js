86d0534de030142ceb37b079b50944fb9557ea69
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-03-07 07:06:49 -0300

    add class .sr-only

--- src/styles/global.js
+++ src/styles/global.js
@@ -59,4 +59,16 @@ body {
     filter: brightness(.9);
   }
 
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border-width: 0;
+  }
+
 `;
