42186c38e25ff08b5691a5580a1165d63c28248c
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-03-07 21:33:52 -0300

    page edit dish

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 
 import { Dish } from "../pages/Dish";
 import { New } from "../pages/New";
+import { Edit } from "../pages/Edit";
 
 export function AppRoutes() {
   return(
     <Routes>
-      <Route path="/" element={<New />}/>
+      
+      <Route path="/" element={<Edit />}/>
       <Route path="/dish/:id" element={<Dish/>}/>
     </Routes>
   )
