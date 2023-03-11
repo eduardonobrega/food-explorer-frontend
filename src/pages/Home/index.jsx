19b25d9ae7015a19b9c0148deb9a3422ed0523fc
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-03-11 07:41:02 -0300

    banner mobile home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,13 +3,24 @@ import { Container } from './styles';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { Card } from '../../components/Card';
+import foots100 from '../../assets/foots-100.svg';
+import foots200 from '../../assets/foots-200.svg';
 
 export function Home() {
   return (
     <Container>
       <Header />
       <main>
-      <img src="" alt="" />
+        <div>
+          <picture>
+            <source media="(max-width: 640px)" srcSet={foots100} />
+            <img src={foots200} alt="" />
+          </picture>
+          <div>
+            <h2>Sabores inigualáveis</h2>
+            <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
+          </div>
+        </div>
         <Card
           dish={{
             image: 'https://github.com/eduardonobrega.png',
@@ -17,7 +28,6 @@ export function Home() {
             description:
               'Presunto de parma e rúcula em  um pão com fermentação natural.',
             price: '25,97',
-            
           }}
         />
       </main>
