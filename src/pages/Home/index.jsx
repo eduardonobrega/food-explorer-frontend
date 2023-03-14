8701a7f3dfd75bbe5a1d761f0325228d651e1660
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-03-14 16:49:26 -0300

    mobile carousel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,6 @@
-import { Container } from './styles';
+import { motion } from 'framer-motion';
+import { useEffect, useState, useRef } from 'react';
+import { Container, Section, Banner } from './styles';
 
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
@@ -7,29 +9,184 @@ import foots100 from '../../assets/foots-100.svg';
 import foots200 from '../../assets/foots-200.svg';
 
 export function Home() {
+  const carousel = useRef();
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    console.log(carousel.current?.scrollWidth, carousel.current?.offsetWidth);
+    setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
+  }, []);
+
   return (
     <Container>
       <Header />
-      <main>
+      <Banner>
+        <picture>
+          <source media="(max-width: 640px)" srcSet={foots100} />
+          <img src={foots200} alt="" />
+        </picture>
         <div>
-          <picture>
-            <source media="(max-width: 640px)" srcSet={foots100} />
-            <img src={foots200} alt="" />
-          </picture>
-          <div>
-            <h2>Sabores inigualáveis</h2>
-            <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
-          </div>
+          <h2>Sabores inigualáveis</h2>
+          <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
         </div>
-        <Card
-          dish={{
-            image: 'https://github.com/eduardonobrega.png',
-            name: 'Torradas de Parma',
-            description:
-              'Presunto de parma e rúcula em  um pão com fermentação natural.',
-            price: '25,97',
-          }}
-        />
+      </Banner>
+      <main>
+        <Section>
+          <h2>Pratos principais</h2>
+
+          <motion.div
+            ref={carousel}
+            className="carousel"
+            whileTap={{ cursor: 'grabbing' }}
+          >
+            <motion.div
+              className="inner"
+              drag="x"
+              dragConstraints={{ right: 0, left: -width }}
+              initial={{ x: 100 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 0.8 }}
+            >
+              <motion.div className="item">
+                <Card
+                  dish={{
+                    image: 'https://github.com/eduardonobrega.png',
+                    name: 'Torradas de Parma',
+                    description:
+                      'Presunto de parma e rúcula em  um pão com fermentação natural.',
+                    price: '25,97',
+                  }}
+                />
+              </motion.div>
+              <motion.div className="item">
+                <Card
+                  dish={{
+                    image: 'https://github.com/eduardonobrega.png',
+                    name: 'Torradas de Parma',
+                    description:
+                      'Presunto de parma e rúcula em  um pão com fermentação natural.',
+                    price: '25,97',
+                  }}
+                />
+              </motion.div>
+              <motion.div className="item">
+                <Card
+                  dish={{
+                    image: 'https://github.com/eduardonobrega.png',
+                    name: 'Torradas de Parma',
+                    description:
+                      'Presunto de parma e rúcula em  um pão com fermentação natural.',
+                    price: '25,97',
+                  }}
+                />
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        </Section>
+
+        <Section>
+          <h2>Refeições</h2>
+          <motion.div
+            ref={carousel}
+            className="carousel"
+            whileTap={{ cursor: 'grabbing' }}
+          >
+            <motion.div
+              className="inner"
+              drag="x"
+              dragConstraints={{ right: 0, left: -width }}
+              initial={{ x: 100 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 0.8 }}
+            >
+              <motion.div className="item">
+                <Card
+                  dish={{
+                    image: 'https://github.com/eduardonobrega.png',
+                    name: 'Torradas de Parma',
+                    description:
+                      'Presunto de parma e rúcula em  um pão com fermentação natural.',
+                    price: '25,97',
+                  }}
+                />
+              </motion.div>
+              <motion.div className="item">
+                <Card
+                  dish={{
+                    image: 'https://github.com/eduardonobrega.png',
+                    name: 'Torradas de Parma',
+                    description:
+                      'Presunto de parma e rúcula em  um pão com fermentação natural.',
+                    price: '25,97',
+                  }}
+                />
+              </motion.div>
+              <motion.div className="item">
+                <Card
+                  dish={{
+                    image: 'https://github.com/eduardonobrega.png',
+                    name: 'Torradas de Parma',
+                    description:
+                      'Presunto de parma e rúcula em  um pão com fermentação natural.',
+                    price: '25,97',
+                  }}
+                />
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        </Section>
+
+        <Section>
+          <h2>Bebidas</h2>
+
+          <motion.div
+            ref={carousel}
+            className="carousel"
+            whileTap={{ cursor: 'grabbing' }}
+          >
+            <motion.div
+              className="inner"
+              drag="x"
+              dragConstraints={{ right: 0, left: -width }}
+              initial={{ x: 100 }}
+              animate={{ x: 0 }}
+              transition={{ duration: 0.8 }}
+            >
+              <motion.div className="item">
+                <Card
+                  dish={{
+                    image: 'https://github.com/eduardonobrega.png',
+                    name: 'Torradas de Parma',
+                    description:
+                      'Presunto de parma e rúcula em  um pão com fermentação natural.',
+                    price: '25,97',
+                  }}
+                />
+              </motion.div>
+              <motion.div className="item">
+                <Card
+                  dish={{
+                    image: 'https://github.com/eduardonobrega.png',
+                    name: 'Torradas de Parma',
+                    description:
+                      'Presunto de parma e rúcula em  um pão com fermentação natural.',
+                    price: '25,97',
+                  }}
+                />
+              </motion.div>
+              <motion.div className="item">
+                <Card
+                  dish={{
+                    image: 'https://github.com/eduardonobrega.png',
+                    name: 'Torradas de Parma',
+                    description:
+                      'Presunto de parma e rúcula em  um pão com fermentação natural.',
+                    price: '25,97',
+                  }}
+                />
+              </motion.div>
+            </motion.div>
+          </motion.div>
+        </Section>
       </main>
       <Footer />
     </Container>
