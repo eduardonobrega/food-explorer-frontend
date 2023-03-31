a9ef867bfd3c373e85aa0f30bc5d7c08b3b1999c
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-03-31 10:41:23 -0300

    edit dish

--- src/pages/Edit/index.jsx
+++ src/pages/Edit/index.jsx
@@ -1,24 +1,122 @@
-import { Container, Form, Textarea } from './styles';
 import { FiChevronLeft, FiUpload } from 'react-icons/fi';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import { api } from '../../services/api';
 
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { LinkText } from '../../components/LinkText';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
+import { Select } from '../../components/Select';
 import { AddIngredients } from '../../components/AddIngredients';
 
+import { Container, Form, Textarea } from './styles';
+
 export function Edit() {
+  const [photoFile, setPhotoFile] = useState(null);
+  const [name, setName] = useState('');
+  const [category, setCategory] = useState('meals');
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState('');
+  const [ingredients, setIngredients] = useState([]);
+  const [newIngredient, setNewIngredient] = useState('');
+
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  async function updateDish() {
+    const notANumber = isNaN(price) || price === '';
+
+    if (!name || price < 0 || notANumber) {
+      return;
+    }
+
+    if (newIngredient != '') {
+      return alert(
+        `Clique no + para adicionar o ingredient tag: ${newIngredient}. ou limpe o campo!`
+      );
+    }
+
+    try {
+      await api.put(`/dishes/${id}`, {
+        name,
+        category,
+        price,
+        description,
+        ingredients,
+      });
+
+      if (photoFile) {
+        const fileUploadForm = new FormData();
+        fileUploadForm.append('photo', photoFile);
+
+        await api.patch(`dishes/photo/${id}`, fileUploadForm);
+      }
+
+      alert('Prato atualizado!');
+      navigate(-1);
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert('Não foi possível atualizar!');
+      }
+    }
+  }
+
+  function handleNewIngredient() {
+    if (newIngredient) {
+      const isNewIngredient = !ingredients.includes(newIngredient);
+      if (isNewIngredient) {
+        setIngredients((prevState) => [...prevState, newIngredient]);
+      } else {
+        alert('Ingredient já Adicionado');
+      }
+    }
+
+    setNewIngredient('');
+    document.getElementById('add').focus();
+  }
+
+  function handleRemoveIngredient(ingredientDeleted) {
+    setIngredients((prevState) =>
+      prevState.filter((ingredient) => ingredient !== ingredientDeleted)
+    );
+  }
+
+  function handleUploadPhoto(event) {
+    const file = event.target.files[0];
+    setPhotoFile(file);
+  }
+
+  useEffect(() => {
+    async function fetchDish() {
+      const response = await api.get(`/dishes/${id}`);
+
+      const dish = response.data;
+
+      setName(dish.name);
+      setIngredients(dish.ingredients.map((ingredient) => ingredient.name));
+      setPrice(dish.price);
+      setDescription(dish.description);
+      setCategory(dish.category);
+    }
+
+    fetchDish();
+  }, []);
+
   return (
     <Container>
       <Header />
-      
+
       <div className="wrapper">
         <LinkText name="voltar" icon={FiChevronLeft} to={-1} />
       </div>
 
       <main>
-        <Form>
+        <Form onSubmit={(e) => e.preventDefault()}>
           <h1>Editar prato</h1>
 
           <div id="threeColumns">
@@ -26,26 +124,39 @@ export function Edit() {
               <label htmlFor="image">Imagem do prato</label>
               <div>
                 <span>
-                  <FiUpload /> Selecione a imagem
+                  <FiUpload />{' '}
+                  {photoFile ? photoFile.name : 'Selecione a imagem'}
                 </span>
                 <Input
                   id="image"
                   accept="image/png, image/jpeg"
                   type="file"
                   style={{ cursor: 'pointer' }}
+                  onChange={handleUploadPhoto}
                 />
               </div>
             </div>
 
-            <Input id="name" label="Nome" placeholder="Salada Ceasar" />
+            <Input
+              id="name"
+              label="Nome"
+              placeholder="Salada Ceasar"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
 
             <div>
               <label htmlFor="category">Categoria</label>
-              <select id="category">
-                <option value="Refeição">Refeição</option>
-                <option value="Pratos principais">Pratos principais</option>
-                <option value="Entrada">Entrada</option>
-              </select>
+              <Select
+                id="category"
+                onChange={(e) => setCategory(e.target.value)}
+                value={category}
+              >
+                <option value="meal">Refeição</option>
+                <option value="dessert">Sobremesa</option>
+                <option value="drink">Bebida</option>
+              </Select>
             </div>
           </div>
 
@@ -53,9 +164,23 @@ export function Edit() {
             <div>
               <label htmlFor="add">Ingredientes</label>
               <div>
-                <AddIngredients value="Pão Naan" size="6" />
-                <AddIngredients value="Feijão" size="4" />
-                <AddIngredients id="add" isNew size="6" />
+                {ingredients.map((ingredient, index) => (
+                  <AddIngredients
+                    key={String(index)}
+                    value={ingredient}
+                    onClick={(e) => handleRemoveIngredient(ingredient)}
+                    size={String(ingredient.length)}
+                  />
+                ))}
+
+                <AddIngredients
+                  id="add"
+                  isNew
+                  size="6"
+                  value={newIngredient}
+                  onChange={(e) => setNewIngredient(e.target.value)}
+                  onClick={handleNewIngredient}
+                />
               </div>
             </div>
 
@@ -63,8 +188,11 @@ export function Edit() {
               id="price"
               type="number"
               label="Preço"
-              placeholder="R$ 40,00"
+              placeholder="R$ 00,00"
               min="0"
+              step="0.010"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
             />
           </div>
 
@@ -73,12 +201,18 @@ export function Edit() {
             <Textarea
               id="description"
               placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
           </div>
 
           <div>
             <Button type="button" id="buttonRemove" title="Excluir prato" />
-            <Button type="button" id="buttonAdd" title="Salvar alterações" />
+            <Button
+              id="buttonAdd"
+              title="Salvar alterações"
+              onClick={updateDish}
+            />
           </div>
         </Form>
       </main>
