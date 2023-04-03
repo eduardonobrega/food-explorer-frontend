c51028b94cb85460df34ceeab82f3ca5096c0aa5
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-04-03 14:04:08 -0300

    photo and description card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,16 +2,20 @@ import { FiHeart } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 import { useAuth } from '../../hooks/auth';
+import { api } from '../../services/api';
 
-import Edit from '../../assets/icons/pencil.svg';
 import { Counter } from '../Counter';
 import { Button } from '../Button';
+import Edit from '../../assets/icons/pencil.svg';
+import photoPlaceholder from '../../assets/photoPlaceholder.png';
 
 import { Container } from './styles';
 
 export function Card({ dish, favorite = false }) {
   const { user } = useAuth();
-
+  const photoUrl = dish.photo
+    ? `${api.defaults.baseURL}/files/${dish.photo}`
+    : photoPlaceholder;
   return (
     <Container>
       {user.isAdmin ? (
@@ -26,11 +30,16 @@ export function Card({ dish, favorite = false }) {
         </button>
       )}
 
-      <Link to="/dish/1">
-        <img src={dish.image} alt={dish.name} />
+      <Link to={`/dish/${dish.id}`}>
+        <img src={photoUrl} alt={dish.name} />
         <h3>{dish.name} &gt;</h3>
         <p>{dish.description}</p>
-        <span>R$ {dish.price}</span>
+        <span>
+          {dish.price.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          })}
+        </span>
       </Link>
       {!user.isAdmin && (
         <div>
