a0b3c918e77e3b4f15e3c6ac2fcb8a9a7bcbb305
eduardo <eduardo.nobrega1999@hotmail.com>
2023-09-18 15:39:46 -0300

    updated baseURL axios

--- src/services/api.js
+++ src/services/api.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const api = axios.create({
   // baseURL: 'http://localhost:3000',
   // baseURL: 'https://food-explorer-backend-8cgn624yz-eduardonobrega.vercel.app/',
-  baseURL: 'https://foodexplorer-api-74ue.onrender.com/',
+  baseURL: 'https://foodexplorer-api-74ue.onrender.com',
 })
